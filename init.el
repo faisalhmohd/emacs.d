e7dc452793c578e4cdec0fd3bb079ca84b1d1d5f
Mohammad Faisal <faisalhmohd@live.com>
2017-04-08 10:49:21 +0600

    Neotree Icons added

--- init.el
+++ init.el
@@ -49,10 +49,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- (set-face-attribute 'default nil
-                    :height 140
-                    :weight 'normal
-                    :width 'normal)
  )
 
 ;; Load theme
@@ -152,3 +148,7 @@
 
 ;; Enable Rainbow Delimiters
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+;; Adding all-the-icons
+(require 'all-the-icons)
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
