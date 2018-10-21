efb47b113a0ca266d8a6ea5217f793446b0add6b
Mohammad Faisal <faisalhmohd@gmail.com>
2018-10-21 11:20:50 +0000

    Add 2 spaces for office and 4 spaces for work

--- init.el
+++ init.el
@@ -29,7 +29,7 @@
 ;; Removing Menu Bar, Toolbar and Scroll Bar
 
 (menu-bar-mode -1)
-(toggle-scroll-bar -1) 
+;; (toggle-scroll-bar -1) 
 (tool-bar-mode -1)
 
 (custom-set-variables
@@ -177,6 +177,7 @@
 (setq-default neo-show-hidden-files t)
 (neotree-toggle)
 (global-set-key [f8] 'neotree-toggle)
+(switch-to-buffer-other-window "*scratch*")
 
 ;; Adding all-the-icons
 (require 'all-the-icons)
@@ -208,4 +209,47 @@
 (add-hook 'yaml-mode-hook '(lambda () (ansible 1)))
 
 ;; Replace tabs with spaces
-(setq-default indent-tabs-mode nil)
+(defun my-setup-indent (n)
+  ;; java/c/c++
+  (setq-local c-basic-offset n)
+  ;; web development
+  (setq-local coffee-tab-width n) ; coffeescript
+  (setq-local javascript-indent-level n) ; javascript-mode
+  (setq-local js-indent-level n) ; js-mode
+  (setq-local js2-basic-offset n) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
+  (setq-local web-mode-markup-indent-offset n) ; web-mode, html tag in html file
+  (setq-local web-mode-css-indent-offset n) ; web-mode, css in html file
+  (setq-local web-mode-code-indent-offset n) ; web-mode, js code in html file
+  (setq-local css-indent-offset n) ; css-mode
+  )
+
+(defun my-office-code-style ()
+  (interactive)
+  (message "Office code style!")
+  ;; use tab instead of space
+  (setq-local indent-tabs-mode nil)
+  ;; indent 4 spaces width
+  (my-setup-indent 2))
+
+(defun my-personal-code-style ()
+  (interactive)
+  (message "My personal code style!")
+  ;; use space instead of tab
+  (setq indent-tabs-mode nil)
+  ;; indent 2 spaces width
+  (my-setup-indent 4))
+
+(defun my-setup-develop-environment ()
+  (interactive)
+  (let ((proj-dir (file-name-directory (buffer-file-name))))
+    (if (string-match-p "personal" proj-dir)
+	(my-personal-code-style))
+
+    (if (string-match-p "nc" proj-dir)
+	(my-office-code-style))))
+
+;; prog-mode-hook requires emacs24+
+(add-hook 'prog-mode-hook 'my-setup-develop-environment)
+;; a few major-modes does NOT inherited from prog-mode
+(add-hook 'lua-mode-hook 'my-setup-develop-environment)
+(add-hook 'web-mode-hook 'my-setup-develop-environment)
